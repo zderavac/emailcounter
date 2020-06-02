14733ea84d2ca9b79b372dec7d406236fbe57238
zderavac <66291244+zderavac@users.noreply.github.com>
2020-06-01 21:17:38 -0700

    Update EmailcounterApplication.java

--- src/main/java/com/minya/emailcounter/EmailcounterApplication.java
+++ src/main/java/com/minya/emailcounter/EmailcounterApplication.java
@@ -6,11 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * 
  * @author Minya
- * TO test run as spring boot application 
  * 
+ * Main applicaion class
+ * 
+ * Run it as spring boot application 
+ * Go to web brower and hit
  * http://localhost:8080/count?emails=test.email@gmail.com,test.email%2Bspam@gmail.com,testemail@gmail.com
- *
+ * expected result is 1
  */
+
 
 @SpringBootApplication
 public class EmailcounterApplication {
