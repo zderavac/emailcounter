43444a5c75b9c5c1dc7ce20f0bff1f99b47422a5
zderavac <66291244+zderavac@users.noreply.github.com>
2020-06-01 21:12:44 -0700

    Update UniqueEmailService.java

--- src/main/java/com/minya/emailcounter/service/UniqueEmailService.java
+++ src/main/java/com/minya/emailcounter/service/UniqueEmailService.java
@@ -3,9 +3,17 @@ package com.minya.emailcounter.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-
+	
+/**
+* 
+* @param emails input emails fro user request
+* @return count of unique emails based on rules specified in properties
+*/
 public interface UniqueEmailService {
 	
+	/**
+	
+	*/
 	public int getCountOfUniqueEmails(List<String> emails);
 
 }
